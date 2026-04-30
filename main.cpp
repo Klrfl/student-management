978b04c4aa3c8cdd0d7b30e6f6e1b695db13ca34
Ren 女 <123250125@student.upnyk.ac.id>
2026-04-30 17:34:27 +0700

    Adding loop and tidy up

--- main.cpp
+++ main.cpp
@@ -3,47 +3,47 @@
 
 
 int main (){
-  std::cout << "=========================\n";
-  std::cout << "Student Management System\n";
-  std::cout << "=========================\n";
-  std::cout << "1. View Student List\n";
-  std::cout << "2. Create Student List\n";
-  std::cout << "3. Update Student List\n";
-  std::cout << "4. Delete Student List\n";
-  std::cout << "5. Exit\n";
-  
-  int input;
-  std::cin >> input;
+	int input;
+	
+	do {
+		std::cout << "=========================\n";
+		std::cout << "Student Management System\n";
+		std::cout << "=========================\n";
+		std::cout << "1. View Student List\n";
+		std::cout << "2. Create Student List\n";
+		std::cout << "3. Update Student List\n";
+		std::cout << "4. Delete Student List\n";
+		std::cout << "5. Exit\n";
+		std::cout << "Masukkan pilihan (1-5): ";
+		std::cin >> input;
+		
+			switch (input) {
+				case 1:{
+					std::cout << "Menu 1" << "\n";
+					break;
+				}
+				case 2:{
+					std::cout << "Menu 2" << "\n";
+					break;
+				}		
+				case 3:{
+					std::cout << "Menu 3" << "\n";
+					break;
+				}
+				case 4:{
+					std::cout << "Menu 4" << "\n";
+					break;
+				}
+				case 5:{
+					std::cout << "Goodbye, Admin!";
+					return 0;
+					break;
+				}
+				default:{
+					std::cout << "Invalid Input\n";
+					break;
+				}
+			}
 
-  switch (input) {
-    case 1:{
-      std::cout << "Menu 1";
-      break;
-    }
-    case 2:{
-
-      std::cout << "Menu 2";
-      break;
-    }
-    case 3:{
-
-      std::cout << "Menu 3";
-      break;
-    }
-    case 4:{
-
-      std::cout << "Menu 4";
-      break;
-    }
-    case 5:{
-
-      std::cout << "Menu 5";
-      break;
-    }
-    default:{
-      std::cout << "Invalid Input\n";
-      break;
-    }
-  }
-
+	} while (input != 5);
 }
