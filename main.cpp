d5630ed30d213c07132ff40090a953b8acbcb8e8
Efraim Munthe <efrayanglain@gmail.com>
2026-05-04 09:41:10 +0700

    formatting

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
-void bubbleSort(int array[], int n){
-	for (int i=0; i < n-1; i++){
-		for (int j=0; j < n-i-1; j++){
-			if (array[j] > array[j+1]){
-				int temp = array[j];
+void bubbleSort(int array[], int size){
+  int temp;
+
+	for (int i=0; i < size-1; i++) {
+		for (int j=0; j < size-i-1; j++) {
+			if (array[j] > array[j+1]) {
+				temp = array[j];
 				array[j] = array[j+1];
 				array[j+1] = temp;
 			}
